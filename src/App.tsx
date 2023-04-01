19714d2f602d00eeb06e6ce53f0fafdd7d2dcd9b
aliakbarvafaei <72415266+aliakbarvafaei@users.noreply.github.com>
2023-04-01 15:15:03 +0430

    Create theme font in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 // import "./assets/scss/style.scss";
 import "./index.css";
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 // import { AuthProvider } from "./contexts/Auth";
 // import { ThemeProvider } from "./contexts/theme";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import { ToastProvider } from "./contexts/ToastState";
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
@@ -11,10 +13,60 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-const App :React.FC = () => (
+const theme = createTheme({
+  typography: {
+    fontFamily: `IRANYekan`,
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        fontFamily: "IRANYekan",
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          fontFamily: "IRANYekan",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontFamily: "IRANYekan",
+        },
+        outlinedSizeSmall: {
+          fontFamily: "IRANYekan",
+        },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          fontFamily: "IRANYekan",
+        },
+      },
+    },
+    MuiInput: {
+      styleOverrides: {
+        root: {
+          fontFamily: "IRANYekan",
+        },
+      },
+    },
+    MuiPagination: {
+      styleOverrides: {
+        root: {
+          fontFamily: "IRANYekan",
+        },
+      },
+    },
+  },
+});
+const App: React.FC = () => (
   <BrowserRouter>
     <Provider store={store}>
-      {/* <ThemeProvider> */}
+      <ThemeProvider theme={theme}>
         {/* <AuthProvider> */}
         <ToastProvider>
           <Switch>
@@ -34,7 +86,7 @@ const App :React.FC = () => (
           </Switch>
         </ToastProvider>
         {/* </AuthProvider> */}
-      {/* </ThemeProvider> */}
+      </ThemeProvider>
     </Provider>
   </BrowserRouter>
 );
